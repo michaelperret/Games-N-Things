c3ea6a20c69d23154f56e7b494378300cf00469c
Michael Perret <michael.t.perret@gmail.com>
2014-08-04 14:27:58 -0700

    fixing shit

--- snake/static/js/snake.js
+++ snake/static/js/snake.js
@@ -10,25 +10,52 @@ $(document).ready(function () {
     var width = $("#canvas").width();
     var height = $("#canvas").height();
     var currentDirection;
+    var speed = 100;
+    var food, food2, food3;
+    var play = true;
+    var highScore = 0;
+    var score;
 
+    //increment speed
+    var incSpeed = function(){
+        if(speed>25){
+            speed = speed-5;
+        }
+    };
 
+    // Create a random piece of food
+    function createFood() {
+        food = {
+            x: Math.round(Math.random() * (width - cellWidth) / cellWidth),
+            y: Math.round(Math.random() * (height - cellWidth) / cellWidth)
+        };
+//
+    }
 
 
     function gameLoop() {
 
         var nextPosition = getNextPosition();
+        if(checkGameOver(nextPosition,snakeBody)){
+            gameOver();
+            return;
+        }
+        var ateFood = checkEatFood(nextPosition);
+        if(!ateFood) {
+            // Remove the tail of the snake, only if we didn't eat food this time around
+            snakeBody.pop();
+        }
 
-        // Remove the tail of the snake
-        snakeBody.pop();
+
 
         // Add the next position
         // to the front of our snakeBody
         snakeBody.unshift(nextPosition);
 
         getNextPosition();
-        checkGameOver();
-        checkEatFood();
+
         paintCanvas();
+
     }
 
     // Get the next position of the snake
@@ -50,11 +77,46 @@ $(document).ready(function () {
     }
 
     // Check if snake has collided with walls or itself
-    function checkGameOver() {}
+    function checkGameOver(position, snakeBody) {
+        if(position.x == -1 || position.x == width / cellWidth) {
+            // If the snake has gone off the left or right boundaries, game over!
+            return true;
+        } else if(position.y == -1 || position.y == height / cellWidth) {
+            // If the snake has gone off the top or bottom boundaries, game over!
+            return true;
+        } else {
+            // If the snake's next position collides with another cell in it's body, game over!
+            for (var i = 0; i < snakeBody.length; i++) {
+                if (snakeBody[i].x == position.x && snakeBody[i].y == position.y) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
 
     // Check if snake is on the same space as food
-    function checkEatFood() {}
+    function checkEatFood(position) {
+        if (position.x == food.x && position.y == food.y) {  // The snake is eating the food
+            // Create a new piece of food, which removes this current one
+            createFood();
+            score++;
+            incSpeed();
+            clearInterval(gameLoopInterval);
+            gameLoopInterval = setInterval(gameLoop, speed);
+            return true;
 
+        }
+//          if(position.x == food2.x && position.y == food2.y){
+//            createFood();
+//            score+=2;
+//            return true
+//        }
+        else {
+            return false;
+        }
+    }
     // Paint the snake and food
 
     function createSnake() {
@@ -70,6 +132,21 @@ $(document).ready(function () {
         }
     }
 
+    function gameOver(){
+        clearInterval(gameLoopInterval);
+
+        if(score>highScore){
+            highScore = score;
+        }
+        play = confirm('GAME OVER BITCHES\nClick "OK" to play again');
+        if(play){
+            startGame();
+        }else{
+            console.log("poop");
+        }
+//        console.log(poop);
+    }
+
 
 
     function paintCanvas() {
@@ -81,8 +158,18 @@ $(document).ready(function () {
         //paint snake
         for (var i = 0; i < snakeBody.length; i++) {
             var cell = snakeBody[i];
-            paintCell(cell.x, cell.y);
+            if(i===0){
+                paintHead(cell.x,cell.y)
+            }else{
+                paintCell(cell.x, cell.y);
+            }
         }
+
+
+
+        var scoreText = "High Score: " + highScore + " Current Score: "+ score;
+        canvasContext.fillText(scoreText, 5, height - 5);
+        paintFood(food.x, food.y);
     }
 
     //paint cells
@@ -92,7 +179,22 @@ $(document).ready(function () {
         canvasContext.strokeStyle = "black";
         canvasContext.strokeRect(x * cellWidth, y * cellWidth, cellWidth, cellWidth);
     }
-     var gameLoopInterval;
+
+    function paintFood(x,y) {
+        canvasContext.fillStyle = "red";
+        canvasContext.fillRect(x * cellWidth, y * cellWidth, cellWidth, cellWidth);
+        canvasContext.strokeStyle = "black";
+        canvasContext.strokeRect(x * cellWidth, y * cellWidth, cellWidth, cellWidth);
+    }
+
+    function paintHead(x,y){
+        canvasContext.fillStyle = "blue";
+        canvasContext.fillRect(x*cellWidth,y*cellWidth,cellWidth,cellWidth);
+        canvasContext.strokeStyle = "black";
+        canvasContext.strokeRect(x*cellWidth,y*cellWidth, cellWidth,cellWidth);
+    }
+
+    var gameLoopInterval;
 
     $(document).keydown(function (e) {
         var key = e.which;
@@ -108,14 +210,28 @@ $(document).ready(function () {
 
 
 
+
+
     function startGame() {
         // Let's set the game loop to run every 60 milliseconds
         createSnake();
+        speed = 100;
         currentDirection = "right";
-        gameLoopInterval = setInterval(gameLoop, 60);
+        createFood();
+        score = 0;
+        clearInterval(gameLoopInterval);
+
+        gameLoopInterval = setInterval(gameLoop, speed);
     }
 
-    startGame();
+    $('#startGame').one('click', function(){
+
+        startGame()
+
+    });
+
+
+
 
 
 });
